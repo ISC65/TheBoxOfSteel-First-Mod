2449829d06a03c9e78da44de7adee6e64ec4eb72
ISC65 <shonajns22@googlemail.com>
2026-01-18 16:21:05 +0000

    New method to register items

--- src/main/java/com/myfirstmod/items/ModItems.java
+++ src/main/java/com/myfirstmod/items/ModItems.java
@@ -1,7 +1,9 @@
 package com.myfirstmod.items;
 
+import com.myfirstmod.SomeTutorialMod;
+
 public class ModItems {
     public static void registerModItems() {
-
+        SomeTutorialMod.LOGGER.info("New Item Added");
     }
 }
